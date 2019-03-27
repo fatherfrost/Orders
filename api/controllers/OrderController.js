a2028c6c4e57282ba2645f8f55984d86766d19dc
Aleksandr <olsnis@pascalium.com>
2019-03-27 15:17:47 +0200

    fixes

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -44,14 +44,16 @@ function cancelOrder(req, res) {
     Order.findOne({name: req.body.name})
         .exec((err, order) => {
             if (err) return res.send({success: false, message: 'Error finding order'});
-            if (order.isDelivered || order.isCancelled) {
-                return res.send({success: false, message: 'Impossible to cancel delivered or cancelled order'});
-            } else {
-                order.isCancelled = true;
-                order.isConfirmed = false;
-                order.save();
-                return res.send({success: true, message: 'Order is canceled.'});
-            }
+            if (!!order) {
+                if (order.isDelivered || order.isCancelled) {
+                    return res.send({success: false, message: 'Impossible to cancel delivered or cancelled order'});
+                } else {
+                    order.isCancelled = true;
+                    order.isConfirmed = false;
+                    order.save();
+                    return res.send({success: true, message: 'Order is canceled.'});
+                }
+            } else return res.send({success: false, message: 'Order does not exist'});
         })
 }
 
@@ -69,17 +71,20 @@ function checkOrder(req, res) {
     Order.findOne({name: req.query.name})
         .exec((err, order) => {
             if (err) return res.send({success: false, message: 'Error finding order'});
-            let status;
-            if (order.isCancelled) {
-                status = 'Order is cancelled';
-            } else if (order.isConfirmed && !order.isDelivered) {
-                status = 'Order is confirmed and in delivery';
-            } else if (order.isConfirmed && order.isDelivered) {
-                status = 'Order is delivered'
-            } else {
-                status = 'Order is created and not confirmed.'
-            }
-            return res.send({order: order, status : status});
+            if (!!order) {
+                let status;
+                if (order.isCancelled) {
+                    status = 'Order is cancelled';
+                } else if (order.isConfirmed && !order.isDelivered) {
+                    status = 'Order is confirmed and in delivery';
+                } else if (order.isConfirmed && order.isDelivered) {
+                    status = 'Order is delivered'
+                } else {
+                    status = 'Order is created and not confirmed.'
+                }
+                return res.send({order: order, status : status});
+            } else return res.send({success: false, message: 'Order does not exist'});
+
         })
 }
 
